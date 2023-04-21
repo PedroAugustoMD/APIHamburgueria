ca94999f279e0fd9ef02637f7364091a18e2e8b0
PedroAugustoMD <pedro.augustohp02@gmail.com>
2023-04-21 00:30:40 -0300

    feat: Criando o usecase de criar cliente #1

---
+++ src/usecases/clientes/createCliente.ts
@@ -0,0 +1,24 @@
+import { PrismaClient, Cliente } from "@prisma/client";
+const prisma = new PrismaClient()
+
+export interface typeMCliente{
+    nome: string
+    email: string
+    senha: string
+    telefone: string
+
+}
+
+export class CriandoCliente{
+    async execute({nome,email,senha,telefone}:typeMCliente): Promise<Cliente>{
+        const novoCliente = await prisma.cliente.create({
+            data:{
+                nome:nome,
+                email:email,
+                senha:senha,
+                telefone:telefone
+            }
+        })
+        return novoCliente
+    }
+}

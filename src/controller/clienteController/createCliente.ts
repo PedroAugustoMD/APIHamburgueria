321f4adfbdab4964a678f296eb2a6776c332cc0c
PedroAugustoMD <pedro.augustohp02@gmail.com>
2023-04-21 00:42:44 -0300

    feat: criando o controller para criar cliente #1

---
+++ src/controller/clienteController/createCliente.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { PrismaClient} from "@prisma/client";
+import { StatusCodes} from "http-status-codes";
+import { CriandoCliente } from "../../usecases/clientes/createCliente";
+
+export const prisma = new PrismaClient;
+
+export class createClienteController{
+    async handle(req: Request, res: Response){
+        const {nome,email,senha,telefone} = req.body;
+        try{
+            if(await prisma.cliente.findUnique({where:{email:email}})){
+                return res.status(StatusCodes.UNAUTHORIZED).send({error:"Cliente já existente"})
+            }
+            const create = new CriandoCliente();
+            //criando cliente
+            const cliente = await create.execute({nome,email,senha,telefone});
+            return res.send({
+                cliente,
+            },);
+        }catch(err){
+            return res.status(StatusCodes.NOT_FOUND).send({error: "Falha no Registro1"})
+        }
+    }
+}

f85bc900261a4dea9989e0fe1da4403915e8af5d
PedroAugustoMD <pedro.augustohp02@gmail.com>
2023-04-21 19:08:45 -0300

    feat:Deletar prato #3

--- src/routes/prato.routes.ts
+++ src/routes/prato.routes.ts
@@ -3,21 +3,21 @@ import { createPratoController  } from "../controller/pratoController/createPrat
 import { listPratoController  } from "../controller/pratoController/listPrato";
 import { findPratoController  } from "../controller/pratoController/findPrato";
 import { updatePratoController  } from "../controller/pratoController/updatePrato";
-//import { deleteClienteController  } from "../controller/clienteController/deleteCliente";
+import { deletePratoController  } from "../controller/pratoController/deletePrato";
 
 const pratoRoutes = Router();
 
 const createItem = new createPratoController();
 const listItem = new listPratoController();
-const findPrato = new findPratoController();
-const updatePrato = new updatePratoController();
-//const deleteClient = new deleteClienteController();
+const findItem = new findPratoController();
+const updateItem = new updatePratoController();
+const deleteItem = new deletePratoController();
 
 pratoRoutes.post("/create", createItem.handle);
 pratoRoutes.get("/list", listItem.handle);
-pratoRoutes.get("/find/:nome", findPrato.handle);
-pratoRoutes.put("/update/:nome", updatePrato.handle);
-//clienteRoutes.delete("/delete/:email", deleteClient.handle);
+pratoRoutes.get("/find/:nome", findItem.handle);
+pratoRoutes.put("/update/:nome", updateItem.handle);
+pratoRoutes.delete("/delete/:nome", deleteItem.handle);
 
 
 export{pratoRoutes};
